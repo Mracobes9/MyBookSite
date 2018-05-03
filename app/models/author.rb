37fd316d0fb039ce15ff76175a343a22a929d659
Mracobes <kac.kurilov@yandex.ru>
2018-05-03 22:47:33 +0300

    Add validation in author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, length: { maximum: 20}, presence: true
+    validates :second_name, length: { maximum: 20}, presence: true
+    validates :middle_name, length: { maximum: 20}, presence: true
 end
