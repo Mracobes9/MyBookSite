8ffd354e0203c433bd4d807af0098be541d8a538
Mracobes <kac.kurilov@yandex.ru>
2018-05-03 13:36:56 +0300

    Signup and Signin is added with devise

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 gem 'rails', '~> 5.1.6'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
+gem 'devise'
 # Use sqlite3 as the database for Active Record
 
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'catalog' => 'static_pages#catalog'
   root 'static_pages#home'
-  get 'signup' => 'static_pages#home'
-  get 'signin' => 'static_pages#home'
-
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
