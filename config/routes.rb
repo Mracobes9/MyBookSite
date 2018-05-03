9d6e0a07878980df1bc8113e10b5c61c5991b2b6
Mracobes <kac.kurilov@yandex.ru>
2018-05-04 01:22:33 +0300

    Create routes, links in catalog and view for book show

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'catalog' => 'static_pages#catalog'
   root 'static_pages#home'
   resources :users, only: [:show]
+  resources :books, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
