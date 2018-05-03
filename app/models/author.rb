e2588f18b21c7c1f2aaf19cc96b950b8a4e74126
Mracobes <kac.kurilov@yandex.ru>
2018-05-03 22:49:39 +0300

    Create relationship between books and authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,6 @@ class Author < ApplicationRecord
     validates :name, length: { maximum: 20}, presence: true
     validates :second_name, length: { maximum: 20}, presence: true
     validates :middle_name, length: { maximum: 20}, presence: true
+
+    has_many :books, dependent: :destroy
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
     validates :name, length: {maximum: 30}, presence: true
+    belongs_to :author
 end
